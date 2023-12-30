391661af996c6b00e11f10ee39dfccfbd9dad5e3
Abdul Rahman <rahmanrazy@gmail.com>
2023-12-30 19:01:38 +0000

    complete the add todo feature

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database:"permalist",
+  password : "insha123",
+  port : 5432
+
+
+}
+
+);
 
 const app = express();
 const port = 3000;
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,7 +27,14 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+async function getItems(){
+  const result =await db.query("SELECT * FROM items");
+  items = result.rows;
+}
+
+app.get("/", async (req, res) => {
+  const item = await getItems();
+  console.log(item);
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
@@ -21,13 +43,23 @@ app.get("/", (req, res) => {
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
+  db.query("INSERT INTO items(title) VALUES($1)",[item]);
   items.push({ title: item });
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+ 
 
-app.post("/delete", (req, res) => {});
+  // const result = db.query("")
+
+ 
+  
+});
+
+app.post("/delete", (req, res) => {
+ 
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
